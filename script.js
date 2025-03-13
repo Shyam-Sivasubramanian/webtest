c126022f51ba0d1f8834f12fe7ea66ca787ced00
Shyam Sivasubramanian <96302076+Shyam-Sivasubramanian@users.noreply.github.com>
2025-03-13 01:26:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+// Start with an initial high z-index value
+let highestZindex = 100;
+
 $(document).ready(function(){
-  // Enable draggable functionality for images
-  $(".draggable").draggable();
+  // Enable draggable functionality for images and update z-index on drag start
+  $(".draggable").draggable({
+    start: function(event, ui) {
+      $(this).css('z-index', ++highestZindex);
+    }
+  });
 });
 
 // Function to handle click events on images.
 function handleClick(contributorName) {
   alert("You clicked on " + contributorName);
-  // Optionally, you can redirect to a detailed page:
+  // Optionally, redirect to another page:
   // window.location.href = "profile.html";
 }
